517c4e268a32aca49023ee00134b17ad4ea92f4a
MynameNghia123 <nghialam1509@gmail.com>
2025-11-26 00:39:29 +0700

    cicd

--- frontend/babel.config.cjs
+++ frontend/babel.config.cjs
@@ -7,4 +7,9 @@ const plugins = [
   ['@babel/plugin-syntax-import-meta'],
 ];
 
+// Add Istanbul plugin for coverage instrumentation
+if (process.env.NODE_ENV === 'test') {
+  plugins.push(['istanbul']);
+}
+
 module.exports = { presets, plugins };
